1a2fd62a871abf6f4c0731ea6df04674c358c58f
何偉銘 <wh249@njit.edu>
2021-08-02 00:27:49 -0400

    d

--- neural_tangents/predict.py
+++ neural_tangents/predict.py
@@ -797,7 +797,8 @@ class gradient_descent_mse_ensemble:
                  t: ArrayOrScalar = None,
                  compute_cov: bool = False,
                  **kernel_fn_test_test_kwargs) -> Dict[str, Gaussian]:
-    print(self.learning_rate,'ojbk')
+    print(self.learning_rate, 'ok')
+    print('ojbk')
     if get is None:
       get = ('nngp', 'ntk')
 
